9f4180f612ce60d476155c042135189bfb34afcf
whoiam0734 <indrawans325@gmail.com>
2022-12-14 14:52:18 +0800

    Bebas

---
+++ help.js
@@ -0,0 +1,332 @@
+
+// disini atur aja kak menu bot nya
+// jangan lupa hapus fitur yg ga penting
+
+var monoSpace = '```'
+
+exports.listmenu = (sender,prefix,ad,namenya,premnya,usernya,romnya,tanggal,jam,no) => {
+return`
+━━━❲ Happy -𝘽𝙊𝙏 ━━━
+
+ 𝗨𝗦𝗘𝗥 𝗜𝗡𝗙𝗢
+ ID : @${sender.split('@')[0]}
+ Nama : ${namenya}
+ Premium : ${premnya}
+
+ 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢
+ Library : Baileys-MD
+ Time : ${jam} WIB
+ Date : ${tanggal}
+ Terdaftar : ${usernya}
+ Room Chat : ${romnya}
+ Total Fitur : 600+
+
+ 𝙈𝘼𝙄𝙉 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}menu
+ ${no++} ➳ ${prefix}iklan
+ ${no++} ➳ ${prefix}rules
+ ${no++} ➳ ${prefix}owner
+ ${no++} ➳ ${prefix}script
+ ${no++} ➳ ${prefix}infobot
+ ${no++} ➳ ${prefix}donasi
+ ${no++} ➳ ${prefix}donate
+ ${no++} ➳ ${prefix}jadibot
+ ${no++} ➳ ${prefix}listjadibot
+ ${no++} ➳ ${prefix}sewabot
+ ${no++} ➳ ${prefix}groupbot
+ ${no++} ➳ ${prefix}ownerinfo
+ ${no++} ➳ ${prefix}infoowner
+
+ 𝙐𝙎𝙀𝙍 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}verify
+ ${no++} ➳ ${prefix}report
+ ${no++} ➳ ${prefix}request
+ ${no++} ➳ ${prefix}transfer
+ ${no++} ➳ ${prefix}menfess
+ ${no++} ➳ ${prefix}buatroom
+ ${no++} ➳ ${prefix}secretchat
+ ${no++} ➳ ${prefix}cekprem
+ ${no++} ➳ ${prefix}daftarprem
+ ${no++} ➳ ${prefix}changename
+
+ 𝙊𝙒𝙉𝙀𝙍 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}error
+ ${no++} ➳ ${prefix}clearerr
+ ${no++} ➳ ${prefix}siaran
+ ${no++} ➳ ${prefix}session
+ ${no++} ➳ ${prefix}resetdb
+ ${no++} ➳ ${prefix}runtime
+ ${no++} ➳ ${prefix}setexif
+ ${no++} ➳ ${prefix}setwm
+ ${no++} ➳ ${prefix}setfooter
+ ${no++} ➳ ${prefix}setppbot
+ ${no++} ➳ ${prefix}addprem
+ ${no++} ➳ ${prefix}delprem
+ ${no++} ➳ ${prefix}bc
+ ${no++} ➳ ${prefix}bctext
+ ${no++} ➳ ${prefix}bcvideo
+ ${no++} ➳ ${prefix}bcaudio
+ ${no++} ➳ ${prefix}bcimage
+ ${no++} ➳ ${prefix}broadcast
+
+ 𝙎𝙏𝙊𝙍𝙀 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}kali 1 2
+ ${no++} ➳ ${prefix}bagi 1 2
+ ${no++} ➳ ${prefix}kurang 1 2
+ ${no++} ➳ ${prefix}tambah 1 2
+ ${no++} ➳ ${prefix}dellist key
+ ${no++} ➳ ${prefix}addlist key@response
+ ${no++} ➳ ${prefix}update key@response
+ ${no++} ➳ ${prefix}done <reply orderan>
+ ${no++} ➳ ${prefix}proses <reply orderan>
+ ${no++} ➳ ${prefix}list <only group>
+ ${no++} ➳ ${prefix}shop <only group>
+ 
+ 𝙂𝙍𝙊𝙐𝙋 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}fitnah
+ ${no++} ➳ ${prefix}delete
+ ${no++} ➳ ${prefix}revoke
+ ${no++} ➳ ${prefix}tagall
+ ${no++} ➳ ${prefix}hidetag
+ ${no++} ➳ ${prefix}setdesc
+ ${no++} ➳ ${prefix}linkgrup
+ ${no++} ➳ ${prefix}infogroup
+ ${no++} ➳ ${prefix}setppgrup
+ ${no++} ➳ ${prefix}setnamegrup
+ ${no++} ➳ ${prefix}group open
+ ${no++} ➳ ${prefix}group close
+ ${no++} ➳ ${prefix}antilink on
+ ${no++} ➳ ${prefix}antilink off
+ ${no++} ➳ ${prefix}welcome on
+ ${no++} ➳ ${prefix}welcome off
+ ${no++} ➳ ${prefix}tiktokauto on
+ ${no++} ➳ ${prefix}tiktokauto off
+ ${no++} ➳ ${prefix}kick @tag
+ ${no++} ➳ ${prefix}demote @tag
+ ${no++} ➳ ${prefix}promote @tag
+ 
+ 𝘼𝙐𝙏𝙊𝘿𝙀𝘾𝙏 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}sadcat
+ ${no++} ➳ ${prefix}translate
+ ${no++} ➳ ${prefix}stalkff
+ ${no++} ➳ ${prefix}stalknpm
+ ${no++} ➳ ${prefix}stalkgithub
+ ${no++} ➳ ${prefix}balikhuruf
+ ${no++} ➳ ${prefix}balikangka
+ ${no++} ➳ ${prefix}besarkecil
+ ${no++} ➳ ${prefix}bilangangka
+
+ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙈𝙀𝙉𝙐
+ ${no++} ➳ ${prefix}tiktok
+ ${no++} ➳ ${prefix}ytmp3
+ ${no++} ➳ ${prefix}ytmp4
+ ${no++} ➳ ${prefix}pinterest
+ ${no++} ➳ ${prefix}mediafire
+ ${no++} ➳ ${prefix}wikimedia
+ ${no++} ➳ ${prefix}soundcloud
+ ${no++} ➳ ${prefix}infogempa
+
+ 𝘾𝙊𝙉𝙑𝙀𝙍𝙏 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}tts
+ ${no++} ➳ ${prefix}ttp
+ ${no++} ➳ ${prefix}ttp2
+ ${no++} ➳ ${prefix}attp
+ ${no++} ➳ ${prefix}attp2
+ ${no++} ➳ ${prefix}tourl
+ ${no++} ➳ ${prefix}upload
+ ${no++} ➳ ${prefix}toimg
+ ${no++} ➳ ${prefix}toimage
+ ${no++} ➳ ${prefix}tomp3
+ ${no++} ➳ ${prefix}toaudio
+ ${no++} ➳ ${prefix}tomp4
+ ${no++} ➳ ${prefix}tovideo
+ ${no++} ➳ ${prefix}emojimix
+ ${no++} ➳ ${prefix}emojmix
+ ${no++} ➳ ${prefix}emojinua
+ ${no++} ➳ ${prefix}mixemoji
+ ${no++} ➳ ${prefix}stiker
+ ${no++} ➳ ${prefix}sticker
+ ${no++} ➳ ${prefix}sgif
+ ${no++} ➳ ${prefix}stikergif
+ ${no++} ➳ ${prefix}stickergif
+ ${no++} ➳ ${prefix}swm
+ ${no++} ➳ ${prefix}stikerwm
+ ${no++} ➳ ${prefix}stickerwm
+ ${no++} ➳ ${prefix}smeme
+ ${no++} ➳ ${prefix}memestiker
+ ${no++} ➳ ${prefix}stikermeme
+ ${no++} ➳ ${prefix}stickermeme
+ ${no++} ➳ ${prefix}takesticker
+ ${no++} ➳ ${prefix}emojinua2
+ ${no++} ➳ ${prefix}mixemoji2
+ ${no++} ➳ ${prefix}emojmix2
+ ${no++} ➳ ${prefix}emojimix2
+
+ 𝙏𝙊𝙊𝙇𝙎 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}spamcall
+ ${no++} ➳ ${prefix}translate
+ ${no++} ➳ ${prefix}ssweb-pc
+ ${no++} ➳ ${prefix}ssweb-hp
+ ${no++} ➳ ${prefix}bitly_short
+ ${no++} ➳ ${prefix}cuttly_short
+ ${no++} ➳ ${prefix}tinyurl_short
+ ${no++} ➳ ${prefix}base32
+ ${no++} ➳ ${prefix}base64
+ ${no++} ➳ ${prefix}debase32
+ ${no++} ➳ ${prefix}debase64
+
+ 𝘽𝙐𝙂𝙑𝙄𝙋 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}sendbug 628xxx
+ ${no++} ➳ ${prefix}philips 628xxx
+ ${no++} ➳ ${prefix}philips2 628xxx
+ ${no++} ➳ ${prefix}philips3 628xxx
+ ${no++} ➳ ${prefix}santet @tag
+ ${no++} ➳ ${prefix}santet2 @tag
+ ${no++} ➳ ${prefix}santet3 @tag
+ ${no++} ➳ ${prefix}virtex 628xxx
+ ${no++} ➳ ${prefix}virtex2 628xxx
+ ${no++} ➳ ${prefix}virtex3 628xxx
+ ${no++} ➳ ${prefix}bug1 628xxx
+ ${no++} ➳ ${prefix}bug2 628xxx
+ ${no++} ➳ ${prefix}bug3 628xxx
+ ${no++} ➳ ${prefix}bug4 628xxx
+ ${no++} ➳ ${prefix}bug5 628xxx
+
+ 𝘼𝙉𝙊𝙉𝙔𝙈𝙊𝙐𝙎 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}buatroom 628xxx
+ ${no++} ➳ ${prefix}room <only owner>
+ ${no++} ➳ ${prefix}stopchat <only room>
+ ${no++} ➳ ${prefix}menfess 628xx|bot|hai
+
+ 𝘼𝙐𝘿𝙄𝙊 𝘾𝙃𝘼𝙉𝙂𝙀𝙍 
+ ${no++} ➳ ${prefix}fat
+ ${no++} ➳ ${prefix}fast
+ ${no++} ➳ ${prefix}slow
+ ${no++} ➳ ${prefix}bass
+ ${no++} ➳ ${prefix}deep
+ ${no++} ➳ ${prefix}tupai
+ ${no++} ➳ ${prefix}robot
+ ${no++} ➳ ${prefix}blown
+ ${no++} ➳ ${prefix}smooth
+ ${no++} ➳ ${prefix}earrape
+ ${no++} ➳ ${prefix}reverse
+ ${no++} ➳ ${prefix}nightcore
+
+ 𝘼𝙐𝘿𝙄𝙊 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+ ${no++} ➳ ${prefix}audio${ad++}
+
+ 𝙒𝘼𝙇𝙇𝙋𝘼𝙋𝙀𝙍 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}milf
+ ${no++} ➳ ${prefix}loli
+ ${no++} ➳ ${prefix}wallml
+ ${no++} ➳ ${prefix}waifu
+ ${no++} ➳ ${prefix}husbu
+ ${no++} ➳ ${prefix}cosplay
+ ${no++} ➳ ${prefix}ppcouple
+ ${no++} ➳ ${prefix}wallpaperislami
+ ${no++} ➳ ${prefix}wallpaperinori
+ ${no++} ➳ ${prefix}wallpaperanime
+ ${no++} ➳ ${prefix}wallpapergamer
+ ${no++} ➳ ${prefix}wallpapermeme
+ ${no++} ➳ ${prefix}wallpaperprogamer
+ ${no++} ➳ ${prefix}wallpaperteknologi
+ ${no++} ➳ ${prefix}wallpapercyber
+
+ 𝘼𝙉𝙄𝙈𝙀 𝙈𝙀𝙉𝙐 
+ ${no++} ➳ ${prefix}cry
+ ${no++} ➳ ${prefix}hug
+ ${no++} ➳ ${prefix}pat
+ ${no++} ➳ ${prefix}bully
+ ${no++} ➳ ${prefix}lick
+ ${no++} ➳ ${prefix}kiss
+ ${no++} ➳ ${prefix}awoo
+ ${no++} ➳ ${prefix}waifu
+ ${no++} ➳ ${prefix}shinobu
+ ${no++} ➳ ${prefix}cuddle
+ ${no++} ➳ ${prefix}megumin
+ ${no++} ➳ ${prefix}slap
+ ${no++} ➳ ${prefix}neko
+ ${no++} ➳ ${prefix}wink
+ ${no++} ➳ ${prefix}dance
+ ${no++} ➳ ${prefix}poke
+ ${no++} ➳ ${prefix}glomp
+ ${no++} ➳ ${prefix}bite
+ ${no++} ➳ ${prefix}nom
+ ${no++} ➳ ${prefix}handhold
+ ${no++} ➳ ${prefix}highfive
+ ${no++} ➳ ${prefix}wave
+ ${no++} ➳ ${prefix}smug
+ ${no++} ➳ ${prefix}smile
+ ${no++} ➳ ${prefix}bonk
+
+ `
+}
+
+exports.rulesBot = () =>{
+return`*──「 RULES-BOT 」──*
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+Jika sudah paham rulesnya
+Ketik *#menu* untuk memulai bot`
+}
+
+exports.donasiBot = (cekName,ucapanWaktu) =>{
+return`──「 *MENU DONATE* 」──
+
+Hi *${cekName}* ${ucapanWaktu} 👋🏻
+
+*Payment pulsa*
+Number: -
+A/N: __
+
+*Payment Dana*
+Number: -
+A/N: xlucifer
+
+${monoSpace}Terimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini ^_^${monoSpace}
+
+──「 *THX FOR YOU* 」──`
+}
+
+exports.infoOwner = () =>{
+return`──「 *INFO OWNER* 」──
+
+ *Data Profil*
+ • *Nama:* Xlucifer
+ • *Support:* Xiart
+
+_iam developer bot whatsapp._
+
+ *Sosial Media*
+ • *Whatsapp:* -
+ • *Youtube:* -
+ • *Github:* -
+ `
+}
